68647b094e6c836cdcc5b5cfffedaa07fb37251a
luisolcmd <alejandrosolanofuentes@gmail.com>
2020-08-04 18:09:28 +0000

    Clase administrador agregada

--- Administrative.cpp
+++ Administrative.cpp
@@ -5,15 +5,26 @@
 #include <sstream>
 #include <iomanip>
 #include "Administrative.h"
-Administrative::(double salary):monthlySalary(salary){}
-Administrative::Administrative(string nomb,string apel,int id,double salar):Person(nomb,apel,id),monthlySalary(salary){}
-Administrative::~Administrative(){}
-void Administrative::setMonthlySalary(double MS){MS=monthlySalary};
-double Administrative::getMonthlySalary(){return monthlySalary;}
-double Administrative::salary(return salary)const{}
 
-string Administrative::toString(
+Administrative::Administrative(double salary):monthlySalary(salary){}
+
+Administrative::Administrative(string nomb,string apel,int id,double salar):Person(nomb,apel,id),monthlySalary(salary){}
+
+Administrative::~Administrative(){ }
+
+void Administrative::setMonthlySalary(double MS){ MS = monthlySalary}
+double Administrative::getMonthlySalary(){ return monthlySalary;}
+
+double Administrative::salary()const{
+  return salary;
+}
+
+string Administrative::toString() const{
   stringstream x;
-  
+
+  x<<"Administrative Information : " <<getNom()<<" "<<getApellido()<<endl;
+  x<<"Doc ID: "<<getId()<<endl;
+  x<<"Monthly Salary: "<<getMonthlySalary()<<endl;
+
   return x.str();
-)const{}
+}
--- Administrative.h
+++ Administrative.h
@@ -7,15 +7,19 @@
 #include "Person.h"
 
 class Administrative: public Person {
-double monthlySalady;
-puclic:
-Administrative(double=0);
-Administrative(string=" ", string=" ", int= 0,double=0);
-~Administrative();
-void setMonthlySalary(double);
-double getMonthlySalary();
-double salary()const;
-virtual string toString() const;
+ private:
+  double monthlySalady;
+
+ puclic:
+  Administrative(double=0);
+  Administrative(string = " ", string=" ", int= 0, double=0);
+  ~Administrative();
+
+  void setMonthlySalary(double);
+  double getMonthlySalary();
+
+  double salary()const;
+  virtual string toString() const;
 };
 
 #endif //LAB02_OOP_ADMINISTRATIVE_H
